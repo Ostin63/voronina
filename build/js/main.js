d2237875abd359ad97a5099838e14d4316a2d64e
Ostin63 <Ostin@63users.noreply.githab.com>
2018-11-03 19:40:37 +0400

    стили кнопок

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,14 @@
-{}
+var navli = $('.nav li a');
+$('.tracked').waypoint(function () {
+  var hash = $(this).attr('id');
+
+  navli.removeClass('show');
+
+  $.each(navli, function () {
+    if ($(this).attr('href').slice(1) == hash) {
+      $(this).addClass('show')
+    }
+  });
+}, {
+    offset: '1%'
+  });
