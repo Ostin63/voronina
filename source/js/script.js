09b7076153b67e36366b412cbc9520c0daf55171
Dmitry <upssq@mail.ru>
2021-05-11 10:54:04 +0300

    Добавил js на отключение модалки

--- source/js/script.js
+++ source/js/script.js
@@ -24,10 +24,18 @@ const toggleTop = document.querySelector('.toggle-menu__item--top');
 const toggleMiddle = document.querySelector('.toggle-menu__item--middle');
 const toggleBottom = document.querySelector('.toggle-menu__item--bottom');
 
-toggleMenu.addEventListener("click", function () {
+toggleMenu.onclick = () => {
   toggleTop.classList.toggle("show-top");
   toggleMiddle.classList.toggle("show-middle");
   toggleBottom.classList.toggle("show-bottom");
   toggleMenu.classList.toggle("toggle-menu__wrapper--show");
   siteList.classList.toggle("active");
-});
+};
+
+siteList.onclick = () => {
+  toggleTop.classList.remove("show-top");
+  toggleMiddle.classList.remove("show-middle");
+  toggleBottom.classList.remove("show-bottom");
+  toggleMenu.classList.remove("toggle-menu__wrapper--show");
+  siteList.classList.remove("active");
+}
